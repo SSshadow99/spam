5d0d2c73392fabfc8507ec13331208a31a73b7b5
SSshadow99 <45866647+SSshadow99@users.noreply.github.com>
2019-09-18 21:53:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,120 +1,185 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-
+const client2 = new Discord.Client();
+const myid = ['318705077734998017'];// ايدي حسابك
+const prefix = ['#'];
+ 
 client.on('ready', () => {
-    console.log('I am ready!');
+   console.log(`----------------`);
+   console.log(`Credit Farmm - Script By : n3k4a `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
 });
-
-
-
-
-var prefix = "1";
-
+ 
+client2.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
+});
+ 
+ 
+ 
 client.on('message', message => {
-
-  if (message.author.bot) return;
-
-  if (!message.content.startsWith(prefix)) return;
-
-
-  let command = message.content.split(" ")[0];
-
-  command = command.slice(prefix.length);
-
-
-  let args = message.content.split(" ").slice(1);
-
-
-// -say
-
-  if (command === "say") {
-
-          message.delete()
-
-    message.channel.sendMessage(args.join(" ")).catch(console.error);
-
-  }
-
-}); 
-
-client.on('message', message => {
-    if(message.content === '#d'){
+    if(message.content === prefix+'d'){
         message.channel.send('#daily')
     }
 });
-
+ 
 client.on('message', message => {
-    if(message.content === '#m'){
+    if(message.content === prefix+'c'){
         message.channel.send('#credits')
     }
 });
-
-client.on('ready', async() => {
-var server = "519890778823196672"; 
-var channel = "623742988996313088";
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#ping ')
-    },305);
-})
-
-
-
-
-
-var prefix = "1";
-
-client2.on('message', message => {
-
+ 
+client.on('message', message => {
+    if(message.content === prefix+'r'){
+        message.channel.send("#rep "+"<@" + myid + ">")
+    }
+});
+ 
+client.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**NEW spam code 2019 by n3k4a  - الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر ر الله اكبر الله اكبر **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+         
+        }
+      }
+});
+ 
+client.on('message', message => {
   if (message.author.bot) return;
-
   if (!message.content.startsWith(prefix)) return;
-
-
+ 
   let command = message.content.split(" ")[0];
-
   command = command.slice(prefix.length);
-
-
+ 
   let args = message.content.split(" ").slice(1);
-
-
-// -say
-
-  if (command === "say") {
-
-          message.delete()
-
-    message.channel.sendMessage(args.join(" ")).catch(console.error);
-
+ 
+ 
+ 
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('name', 'SHADOW'); //لازم تعمل الرتبه دي في السيرفر  Role.n3k4a
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
   }
-
-}); 
-
+});
+ 
 client2.on('message', message => {
-    if(message.content === '#d'){
+    if(message.content === prefix+'d'){
         message.channel.send('#daily')
     }
 });
-
+ 
 client2.on('message', message => {
-    if(message.content === '#m'){
+    if(message.content === prefix+'c'){
         message.channel.send('#credits')
     }
 });
+ 
+client2.on('message', message => {
+    if(message.content === prefix+'r'){
+        message.channel.send("#rep "+"<@" + myid + ">")
+    }
+});
+ 
+client2.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**NEW spam code 2019 by n3k4a  - الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر ر الله اكبر الله اكبر **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+         
+        }
+      }
+});
+ 
+client2.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+ 
+ 
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('name', 'SHADOW');
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+});
 
-client2.on('ready', async() => {
-var server = "519890778823196672"; 
-var channel = "623742988996313088";
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#ping ')
-    },305);
-})
 
 
-
-
-
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
-client2.login(process.env.BOT_TOKEN2);
+ 
+client3.on('message', message => {
+    if(message.content === prefix+'d'){
+        message.channel.send('#daily')
+    }
+});
+ 
+client3.on('message', message => {
+    if(message.content === prefix+'c'){
+        message.channel.send('#credits')
+    }
+});
+ 
+client3.on('message', message => {
+    if(message.content === prefix+'r'){
+        message.channel.send("#rep "+"<@" + myid + ">")
+    }
+});
+ 
+client3.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send(`**NEW spam code 2019 by n3k4a  - الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر الله اكبر ر الله اكبر الله اكبر **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+         
+        }
+      }
+});
+ 
+client3.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+ 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+ 
+  let args = message.content.split(" ").slice(1);
+ 
+ 
+ 
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('name', 'SHADOW'); //لازم تعمل الرتبه دي في السيرفر  Role.n3k4a
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+});
+ 
+client.login(process.env.TOKEN);
+client2.login(process.env.TOKEN2);
+client3.login(process.env.TOKEN2);
